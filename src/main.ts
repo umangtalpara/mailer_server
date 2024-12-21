c1a786798dd0eb309169585f536f22bf934df818
umang <umang.t@frendy.in>
2024-12-21 16:02:46 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config(); // Load .env variables
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
